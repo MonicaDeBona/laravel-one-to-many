98a2c39bb1d958b5c01d4873df4980185ab77bdd
MonicaDeBona <debona.monica@outlook.it>
2023-02-28 17:43:02 +0100

    add migration, model and seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,11 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
-use Illuminare\Support\Facades\Storage;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectSeeder extends Seeder
 {
@@ -18,8 +19,11 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
+
         for ($i = 0; $i < 100; $i++) {
             $newProject = new Project();
+            $newProject->type_id = Type::inRandomOrder()->first()->id;
             $newProject->title = $faker->unique()->sentence(3);
             $newProject->slug = Str::slug($newProject->title);
             $newProject->author = $faker->name();
